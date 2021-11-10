2a850533f72ffb7f38c157e242a76664064004c1
Cubz427 <Flanders911@gmail.com>
2021-11-10 17:05:56 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var buttons = document.getElementsByClassName("button");
+var activeSheet = document.getElementById("active-stylesheet");
+
+//This tests if localStorage already has a value stored inside
+
+if (localStorage.getItem("lastActiveSheet")) {
+     activeSheet.setAttribute("href", localStorage.getItem("lastActiveSheet"));
+}
+
+// Assign the event lister to each button
+for (var i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", switchStyle);  
+}
+
+
+function switchStyle() {
+   var selectedSheet = this.getAttribute("data-stylesheet");
+   activeSheet.setAttribute("href", selectedSheet);
+   localStorage.setItem("lastActiveSheet", selectedSheet);
+}
